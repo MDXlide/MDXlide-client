fe6c6795c3731e799b4fa7dd1a2c6d2beab5934c
lin01-cloud <lin01.dev@gmail.com>
2023-04-14 14:57:53 +0900

    style: slide 추가 버튼 생성 및 스타일링

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import styles from "@/styles/Editor.module.css";
-
-// import Image from "next/image";
-import { Inter } from "next/font/google";
-
 import Nav from "@/components/Nav.js";
 import CodeEditor from "@/components/CodeEditor";
-
-const inter = Inter({ subsets: ["latin"] });
+import CodeSlide from "@/components/CodeSlide";
 
 export default function Editor() {
   return (
-    <>
+    <main>
       <Nav />
-      <CodeEditor />
-    </>
+      <div className={styles.slideEditorWrapper}>
+        <CodeEditor />
+        <CodeSlide />
+        <button className={styles.rightBtn}>+</button>
+        <button className={styles.bottomBtn}>+</button>
+      </div>
+    </main>
   );
 }
