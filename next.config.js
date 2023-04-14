dc8c31c258beb162d7b92b645358bbba630e76ee
lin01-cloud <lin01.dev@gmail.com>
2023-04-14 14:46:10 +0900

    feat: codeEditor redux 연결 및 스타일링

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins");
+const withMDX = require("@next/mdx")();
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([withMDX], {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@mdx-js/loader",
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
+});
--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,34 +1,23 @@
-import { useState, useEffect, Fragment, createElement } from "react";
-import { MDXProvider } from "@mdx-js/react";
-import codeStyles from "@/styles/CodeEditor.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import codeStyles from "@/styles/CodeEditor.module.css";
 import { setSlideText } from "../features/slideSlice";
 
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
-import rehypeParse from "rehype-parse/lib";
-import rehypeReact from "rehype-react/lib";
-
-import CodeSlide from "./CodeSlide";
-
 export default function CodeEditor() {
+  const { text } = useSelector((state) => state.slide);
   const dispatch = useDispatch();
 
   return (
     <>
-      <section className={codeStyles.section}>
-        <div className="title">
+      <section className={codeStyles.codeEditor}>
+        <div className={codeStyles.title}>
           <h3>MDX CODE</h3>
           <span>what mdx?</span>
         </div>
-        <textarea onChange={(e) => dispatch(setSlideText(e.target.value))} />
+        <textarea
+          onChange={(e) => dispatch(setSlideText(e.target.value))}
+          value={text}
+        />
       </section>
-      {/* <MDXProvider>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </MDXProvider> */}
-      <CodeSlide />
     </>
   );
 }
